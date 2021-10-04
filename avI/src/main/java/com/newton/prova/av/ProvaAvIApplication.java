443f0fd20c1faf0e56f7f53373cbe78c9682e91c
DanielRabeloChaves <danielrabelochaves1999@gmail.com>
2021-10-04 20:51:54 -0300

    ProvaAv1ArquiteturaWeb

---
+++ avI/src/main/java/com/newton/prova/av/ProvaAvIApplication.java
@@ -0,0 +1,13 @@
+package com.newton.prova.av;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaAvIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaAvIApplication.class, args);
+	}
+
+}
---
+++ avI/src/main/java/com/newton/prova/av/controller/ProcessoController.java
@@ -0,0 +1,71 @@
+package com.newton.prova.av.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class ProcessoController {
+
+	private final ProcessoService processoService;
+	
+	public ProcessoController(ProcessoService processoService) {
+		this.processoService = processoService;
+	}
+	
+	@GetMapping("/processo/{id}")
+	public ProcessoDto getById(@PathVariable String id) {
+		var processo = ProcessoService.get(id);
+		
+		return new ProcessoDto(processo);
+	}
+	
+	@GetMapping("/processos")
+	public List<ProcessoDto> getAll() {
+		var processos = processoService.getAll();
+		
+		var processoDto = new ArrayList<ProcessoDto>();
+		
+		for (var processo: processos) {
+			processosDto.add(new ProcessoDto(processo));
+		}
+		
+		return processosDto;
+	}
+	
+	@PostMapping("/processos")
+	public ProcessoDto create(@RequestBody ProcessoDto p) {
+		var processo = new Processo(p); 
+		
+		processo = processoService.create(processo);
+		
+		return new ProcessoDto(processo); 
+	}
+	
+	@PutMapping("/processos/{id}")
+	public ProcessoDto update(@PathVariable String id, @RequestBody ProcessoDto p) {
+		var processo = new Processo(p);
+
+		processo = processoService.update(id, processo);
+		
+		return new ProcessoDto(processo);
+	}
+	
+	@DeleteMapping("/processos/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		processoService.delete(id);
+		
+		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+	}
+}
